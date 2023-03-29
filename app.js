747494ef724bca594e4f06d13a80b5a52e9dcca7
Lovis Tawiah <69266504+lovistawiah@users.noreply.github.com>
2023-03-29 10:31:45 +0000

    modified app.js

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const port = 3000;
 
 app.get('/', (req, res) => {
 	res.send('hello, World');
-
 });
-
+app.get('/demo',(req, res)=>{
+	res.set("X-full","Lovis Tawiah");
+	res.status(418);
+	res.send("I prefer coffee");
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
